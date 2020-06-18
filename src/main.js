f100c4adec556c44523f0501afbd93702cefcb3d
danteasc4 <zexovon@gmail.com>
2020-06-18 19:50:34 -0400

    New build

--- src/main.js
+++ src/main.js
@@ -3,37 +3,46 @@ import 'regenerator-runtime/runtime';
 document.addEventListener('DOMContentLoaded', function() {
   let elems = document.querySelectorAll('.collapsible');
   let instances = M.Collapsible.init(elems, {accordian: true});
-  let day1 = M.Collapsible.getInstance(elems[0]);
-
-  // const day1Head = document.getElementById('day--header');
-  // let dayState = day1Head.dataset.state;
-  // const day1Cards = document.querySelectorAll('.day-');
-  // day1Head.addEventListener('click', () => {
-  //   if(dayState === 'closed'){
-  //     anime({
-  //       targets: day1Cards,
-  //       rotateX: [0,360],
-  //       delay: anime.stagger(150),
-  //       duration: 500,
-  //       easing: 'linear'
-  //     });
-  //     dayState = 'open';
-  //   } else {
-  //     dayState = 'closed';
-  //   }
-  // });
-
-  let all5Days = document.getElementsByTagName('li');
-
 
   async function delNodes(nList){
     await nList.forEach(n => n.remove());
   }
-  const day1Links = document.querySelectorAll('.day- > .card-action > a');
-  let today = new Date();
 
-  if(!(today.getDate() !== 12 && today.getMonth() !== 5)){
-    delNodes(day1Links);
+  const day1Links = document.querySelectorAll('.day-1 > .card-action > a');
+  const day2Links = document.querySelectorAll('.day-2 > .card-action > a');
+  const day3Links = document.querySelectorAll('.day-3 > .card-action > a');
+  const day4Links = document.querySelectorAll('.day-4 > .card-action > a');
+  const day5Links = document.querySelectorAll('.day-5 > .card-action > a');
+  let now = new Date();
+  let today = now.getDate();
+  let thisMonth = now.getMonth();
+  console.log(today, thisMonth);
+  console.log((today !== 16 && thisMonth !== 5));
+
+  if(thisMonth === 5){
+    if(today < 22){
+      delNodes(day1Links);
+      delNodes(day2Links);
+      delNodes(day3Links);
+      delNodes(day4Links);
+      delNodes(day5Links);
+    } else if(today < 23) {
+      delNodes(day2Links);
+      delNodes(day3Links);
+      delNodes(day4Links);
+      delNodes(day5Links);
+    } else if(today < 24) {
+      delNodes(day3Links);
+      delNodes(day4Links);
+      delNodes(day5Links);
+    } else if(today < 25) {
+      delNodes(day4Links);
+      delNodes(day5Links);
+    } else if(today < 26) {
+      delNodes(day5Links);
+    } else {
+      // do nothing
+    }
   }
 
   const activities = document.querySelectorAll('.sched-item > .card-content');
@@ -42,6 +51,5 @@ document.addEventListener('DOMContentLoaded', function() {
     let increase = 60*((actSize / 15)-1);
     act.style.height = `${120+increase}px`;
   }
-  // console.log(activities);
 
 });
